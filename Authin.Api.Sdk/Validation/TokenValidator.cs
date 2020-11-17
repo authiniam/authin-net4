c111e8264569827694c88614de0f874477bc6bf1
Mohammad Hosein Mazinani <mhmazinani1994@gmail.com>
2020-11-17 18:57:27 +0330

    Add logout token valiation

--- Authin.Api.Sdk/Validation/TokenValidator.cs
+++ Authin.Api.Sdk/Validation/TokenValidator.cs
@@ -14,8 +14,8 @@ namespace Authin.Api.Sdk.Validation
             RSACryptoServiceProvider cryptoServiceProvider = new RSACryptoServiceProvider();
             cryptoServiceProvider.ImportParameters(new RSAParameters()
             {
-                Modulus = TokenValidator.FromBase64Url(jwks.Keys[0].Modulus),
-                Exponent = TokenValidator.FromBase64Url(jwks.Keys[0].Exponent)
+                Modulus = FromBase64Url(jwks.Keys[0].Modulus),
+                Exponent = FromBase64Url(jwks.Keys[0].Exponent)
             });
             string json;
             try
@@ -30,8 +30,46 @@ namespace Authin.Api.Sdk.Validation
 
             //Ignore other validations in case of refresh token
             if (claims["token_type"].ToString().Equals("refresh_token", StringComparison.OrdinalIgnoreCase))
+            {
                 return claims;
+            }
 
+            if (claims["token_type"].ToString().Equals("logout_token", StringComparison.OrdinalIgnoreCase))
+            {
+                ValidateLogoutToken(claims, issuer, audience);
+            }
+            else //if token type is access_token or id_token
+            {
+                ValidateAccessToken(claims, issuer, audience);
+            }
+
+            return claims;
+        }
+
+        private static void ValidateLogoutToken(JObject claims, string issuer, string audience)
+        {
+            if (!claims.Properties().Any(c => c.Name.Equals("iss")) || !claims["iss"].ToString().Equals(issuer, StringComparison.OrdinalIgnoreCase))
+                throw new TokenValidationException("Invalid issuer.");
+
+            if (!claims.Properties().Any(c => c.Name.Equals("aud")) || !claims["aud"].ToString().Equals(audience, StringComparison.OrdinalIgnoreCase))
+                throw new TokenValidationException("Invalid audience.");
+
+            if (!claims.Properties().Any(c => c.Name.Equals("sub")) && !claims.Properties().Any(c => c.Name.Equals("sid")))
+                throw new TokenValidationException("No sub or sid claim found.");
+
+            if (claims.Properties().Any(c => c.Name.Equals("nonce")))
+                throw new TokenValidationException("Logout token must not contains nonce claim.");
+
+            if (!claims.Properties().Any(c => c.Name.Equals("events")) || !((JProperty)claims["events"].First).Name.Equals("http://schemas.openid.net/event/backchannel-logout"))
+                throw new TokenValidationException("Invalid events.");
+
+            var totalSeconds = DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+            if (!claims.Properties().Any(c => c.Name.Equals("exp")) || long.Parse(claims["exp"].ToString()) <= totalSeconds)
+                throw new TokenValidationException("Expired token.");
+        }
+
+        private static void ValidateAccessToken(JObject claims, string issuer, string audience)
+        {
             if (!claims.Properties().Any(c => c.Name.Equals("iss")) || !claims["iss"].ToString().Equals(issuer, StringComparison.OrdinalIgnoreCase))
                 throw new TokenValidationException("Invalid issuer.");
 
@@ -41,8 +79,6 @@ namespace Authin.Api.Sdk.Validation
             double totalSeconds = DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
             if (!claims.Properties().Any(c => c.Name.Equals("exp")) || long.Parse(claims["exp"].ToString()) <= totalSeconds)
                 throw new TokenValidationException("Expired token.");
-
-            return claims;
         }
 
         private static byte[] FromBase64Url(string base64Url)
